3015bb3555448b346f6efb099f678668ad06c2a9
cftgyhu <gs1gabed254@hotmail.com>
2024-05-05 04:13:27 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function wordBreak(s, wordDict) {
-  const wordSet = new Set(wordDict);
-  const dp = new Array(s.length + 1).fill(false);
-  dp[0] = true;
-  for (let end = 1; end <= s.length; end++) {
-    for (let start = 0; start < end; start++) {
-      if (dp[start] && wordSet.has(s.substring(start, end))) {
-        dp[end] = true;
-        break;
-      }
+function uniquePathsWithObstacles(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = Array.from(Array(m), () => Array(n).fill(0));
+  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) continue;
+      if (i > 0) dp[i][j] += dp[i - 1][j];
+      if (j > 0) dp[i][j] += dp[i][j - 1];
     }
   }
-  return dp[s.length];
+  return dp[m - 1][n - 1];
 }
