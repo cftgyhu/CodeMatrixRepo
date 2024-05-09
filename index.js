de52c41ba0fd49e9d721d1c6b9945f59f661e276
cftgyhu <gs1gabed254@hotmail.com>
2024-05-09 17:19:50 +0700

    feat project

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const fruits = ["apple", "banana", "orange"];
-for (const fruit of fruits) {
-  console.log(fruit);
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    extendPalindrome(i, i);
+    extendPalindrome(i, i + 1);
+  }
+  return count;
+  function extendPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
+    }
+  }
 }
